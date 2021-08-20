b39af374967267450890f30a44e43d977c541338
leninyenni <myseleniumwork@gmail.com>
2021-08-20 19:11:23 +0530

    Changes in Local Only

--- src/packageOne/fileONe.java
+++ src/packageOne/fileONe.java
@@ -5,5 +5,7 @@ public class fileONe {
         System.out.println("first file");
         System.out.println("First change from remote master");
         System.out.println("Second change from remote master");
+        System.out.println("Third change from local master");
+        System.out.println("Fourth change from local master");
     }
 }
