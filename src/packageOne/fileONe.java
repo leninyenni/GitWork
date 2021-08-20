9ad351100b63f16f1add9179281b9d51bd2e1e37
leninyenni <66212934+leninyenni@users.noreply.github.com>
2021-08-20 19:04:27 +0530

    Update fileONe.java

--- src/packageOne/fileONe.java
+++ src/packageOne/fileONe.java
@@ -3,6 +3,7 @@ package packageOne;
 public class fileONe {
     public static void main(String[] args) {
         System.out.println("first file");
-        System.out.println("change from remote master");
+        System.out.println("First change from remote master");
+        System.out.println("Second change from remote master");
     }
 }
